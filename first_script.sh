ddabb4f3dd7e4e14d1779097cbf604b75245687b
root <root@DESKTOP-TNQ0B2T>
2023-06-13 21:28:44 +0530

    2nd details form script

--- first_script.sh
+++ first_script.sh
@@ -1,4 +1,15 @@
-echo "first_scripting"
-echo "modify_using_local"
-echo $BASH
-
+echo "2_scripting"
+echo "----branch testing---"
+name="bot"
+echo " welcome! my name is $name"
+sleep 3
+echo "enter your name"
+read clientname
+echo "hello $clientname"
+sleep 3
+echo "enter your age"
+read clientage
+echo "----your details----"
+sleep 3
+echo "name=$clientname"
+echo "age=$clientage"
